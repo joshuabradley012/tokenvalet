373c2f91154ef4f21aa08eece2a72440d4f31fcc
joshuabradley012 <joshuabradley012@gmail.com>
2021-09-23 09:30:09 -0700

    Fine tuning typography and forms

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,52 +1,10 @@
 import React from 'react';
-import Button from '../button';
+import Ui from '../ui';
 import './style.scss';
 
 const App = () => {
   return (
-    <>
-      <h1>Hello world!</h1>
-      <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
-      <h2>Hello world!</h2>
-      <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
-      <h3>Hello world!</h3>
-      <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
-      <h4>Hello world!</h4>
-      <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
-      <h5>Hello world!</h5>
-      <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
-      <h6>Hello world!</h6>
-      <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
-      <hr />
-      <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
-      <ul>
-        <li>This is an unordered list item</li>
-        <li>This is an unordered list item</li>
-        <li>This is an unordered list item</li>
-        <li>This is an unordered list item</li>
-      </ul>
-      <ol>
-        <li>This is an ordered list item</li>
-        <li>This is an ordered list item</li>
-        <li>This is an ordered list item</li>
-        <li>This is an ordered list item</li>
-        <li>This is an ordered list item</li>
-      </ol>
-      <blockquote>
-        And this is a blockquote!
-        <cite>Andrew Jackson</cite>
-      </blockquote>
-      <hr />
-      <label>First</label>
-      <input type="text" />
-      <label>Second</label>
-      <input type="text" />
-      <label>Third</label>
-      <input type="text" />
-      <label>Fourth</label>
-      <input type="text" />
-      <Button href="#">Submit</Button>
-    </>
+    <Ui />
   );
 };
 
---
+++ client/components/ui/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Button from '../button';
+
+const Ui = () => (
+  <>
+    <h1>Hello world!</h1>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <h2>Hello world!</h2>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <h3>Hello world!</h3>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <h4>Hello world!</h4>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <h5>Hello world!</h5>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <h6>Hello world!</h6>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <hr />
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <ul>
+      <li>This is an unordered list item</li>
+      <li>This is an unordered list item</li>
+      <li>This is an unordered list item</li>
+      <li>This is an unordered list item</li>
+    </ul>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <ol>
+      <li>This is an ordered list item</li>
+      <li>This is an ordered list item</li>
+      <li>This is an ordered list item</li>
+      <li>This is an ordered list item</li>
+      <li>This is an ordered list item</li>
+    </ol>
+    <p>This is some copy in a paragraph tag. Yes it is. This is some copy in a paragraph tag. Yes it is.</p>
+    <blockquote>
+      And this is a blockquote! It should be longer though, quotes are always lengthy expositions meant to highlight oneself, not the subject.
+      <cite>Andrew Jackson</cite>
+    </blockquote>
+    <hr />
+    <label>First</label>
+    <input type="text" />
+    <label>Second</label>
+    <input type="text" />
+    <label>Third</label>
+    <input type="text" />
+    <label>Fourth</label>
+    <input type="text" />
+    <Button href="#">Submit</Button>
+  </>
+);
+
+export default Ui;
+
