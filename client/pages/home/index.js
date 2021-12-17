7091b78c9d721a6f7098a10d29e03c3f2c4a2300
joshuabradley012 <joshuabradley012@gmail.com>
2021-12-16 21:29:06 -0800

    Small CSS changes

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -37,6 +37,59 @@ const Home = () => (
               }
             })}
           </div>
+          <div className="guest-list">
+            <h2>Guest queue</h2>
+            <div className="guest">
+              <div className="header">
+                <h3>John Doe</h3>
+                <span className="date">11/21/2021</span>
+              </div>
+              <div className="body">
+                <span className="vehicle">Red Honda Civic</span>
+                <span className="license">6YYM091</span>
+              </div>
+            </div>
+            <div className="guest">
+              <div className="header">
+                <h3>John Doe</h3>
+                <span className="date">11/21/2021</span>
+              </div>
+              <div className="body">
+                <span className="vehicle">Red Honda Civic</span>
+                <span className="license">6YYM091</span>
+              </div>
+            </div>
+            <div className="guest">
+              <div className="header">
+                <h3>John Doe</h3>
+                <span className="date">11/21/2021</span>
+              </div>
+              <div className="body">
+                <span className="vehicle">Red Honda Civic</span>
+                <span className="license">6YYM091</span>
+              </div>
+            </div>
+            <div className="guest">
+              <div className="header">
+                <h3>John Doe</h3>
+                <span className="date">11/21/2021</span>
+              </div>
+              <div className="body">
+                <span className="vehicle">Red Honda Civic</span>
+                <span className="license">6YYM091</span>
+              </div>
+            </div>
+            <div className="guest">
+              <div className="header">
+                <h3>John Doe</h3>
+                <span className="date">11/21/2021</span>
+              </div>
+              <div className="body">
+                <span className="vehicle">Red Honda Civic</span>
+                <span className="license">6YYM091</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
