e5aa2a4e2a38cfc5ed40264669e6dc11d5aad19d
joshuabradley012 <joshuabradley012@gmail.com>
2021-12-23 22:07:49 -0800

    Fixed clone

--- client/lib/clone.js
+++ client/lib/clone.js
@@ -8,19 +8,9 @@ const clone = object => {
     object.forEach((value, key) => copy.set(clone(key), clone(value)));
     return copy;
 
-  } else if (object instanceof WeakMap) {
-    const copy = new WeakMap();
-    object.forEach((value, key) => copy.set(clone(key), clone(value)));
-    return copy;
-
   } else if (object instanceof Set) {
     const copy = new Set();
-    object.forEach((value, key) => copy.set(clone(key), clone(value)));
-    return copy;
-
-  } else if (object instanceof WeakSet) {
-    const copy = new WeakSet();
-    object.forEach((value, key) => copy.set(clone(key), clone(value)));
+    object.forEach((value, key) => copy.add(clone(key), clone(value)));
     return copy;
 
   } else if (object instanceof Date) {
@@ -28,6 +18,9 @@ const clone = object => {
 
   } else if (object instanceof RegExp) {
     return new RegExp(object);
+
+  } else if (object instanceof Function) {
+    return object.bind({});
 
   } else if (typeof object === 'object') {
     const copy = {};
@@ -39,3 +32,4 @@ const clone = object => {
 };
 
 export default clone;
+
