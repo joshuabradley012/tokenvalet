47bc143766085fedee662ab3544d15aabdf1e854
joshuabradley012 <joshuabradley012@gmail.com>
2021-12-24 20:36:49 -0800

    Added Section

--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -4,33 +4,28 @@ import {
   Button,
   Link,
   Logo,
+  Section,
 } from 'components';
 
 const Login = () => (
-  <section className="login-section">
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-content">
-          <form>
-            <div className="form-header">
-              <Logo />
-              <h2>Sign in</h2>
-              <p>Use your tokenvalet account</p>
-            </div>
-            <label>Email</label>
-            <input type="text" />
-            <label>Password</label>
-            <input type="password" className="password" />
-            <Link href="/reset-password">Forgot password?</Link>
-            <p className="form-group form-footer">
-              <Link href="/signup">Create account</Link>
-              <input type="submit" />
-            </p>
-          </form>
-        </div>
+  <Section className="ticket-section" backButton={false}>
+    <form>
+      <div className="form-header">
+        <Logo />
+        <h2>Sign in</h2>
+        <p>Use your tokenvalet account</p>
       </div>
-    </div>
-  </section>
+      <label>Email</label>
+      <input type="text" />
+      <label>Password</label>
+      <input type="password" className="password" />
+      <Link href="/reset-password">Forgot password?</Link>
+      <p className="form-group form-footer">
+        <Link href="/signup">Create account</Link>
+        <input type="submit" />
+      </p>
+    </form>
+  </Section>
 );
 
 export default Login;
